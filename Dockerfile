844cca7d2be1ea555be4efe10eb5276563e12a80
Yuelin Yan <yuelinyan@gmail.com>
2017-07-10 17:37:10 -0600

    refactor and add a feature to only send notification during certain hours.
    add phantomjs package to source code
    
    add scripts to build docker images
    
    change timezone in dockerfile
    
    add scripts to build and restart docker containers
    
    add time info to log
    
    create build-all script
    
    always restart containers
    
    Get rid of TypeError from ksl returned data
    
    change to multiple queries

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,12 @@ RUN apt-get update \
   && apt-get install build-essential chrpath libssl-dev libxft-dev -y \
   && apt-get install libfreetype6 libfreetype6-dev -y \
   && apt-get install libfontconfig1 libfontconfig1-dev -y
-RUN wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+# RUN wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 RUN tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 \
   && mv phantomjs-2.1.1-linux-x86_64 /usr/local/share \
   && ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
+RUN unlink /etc/localtime \
+  && ln -s /usr/share/zoneinfo/America/Denver /etc/localtime
 
 WORKDIR /app
 
--- ksl.js
+++ ksl.js
@@ -1,15 +1,19 @@
 var Botkit = require('botkit');
-// var os = require('os');
 var path = require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs-prebuilt');
 var binPath = phantomjs.path;
 var fs = require('fs');
 var properties = require ('properties');
+var async = require('async');
+var sleep = require('sleep');
+
 var url_query;
 var childArgs;
 var interval;
 var sendNotification = false;
+var allowNotificationHourStart = 19;
+var allowNotificationHourEnd = 20;
 
 var controller = Botkit.slackbot({
   debug: false,
@@ -26,47 +30,107 @@ if (!fs.existsSync(itemfile)) {
 }
 
 var newItemsfile = __dirname + '/newItems.json';
+var queryFile = __dirname + '/query.json';
 var newItems = {};
 if (!fs.existsSync(newItemsfile)) {
   fs.writeFileSync(newItemsfile, '{}');
 }
+else {
+  newItems = JSON.parse(fs.readFileSync(newItemsfile, 'utf8'));
+}
 
+function sendSlackNotification(item){
+  bot.say(
+    {
+      // text: JSON.stringify(newItems[obj]),
+      text: item.title + ' - ' + item.price + ' - ' + item.age + ' - '
+      + item.mileage + ' - ' + 'http://www.ksl.com/auto/listing/'+item.data.id,
+      channel: 'U2EDZ9C3G' //ylyan
+    }
+  );
+}
+
+function checkToSendNotificationNow(currentHour){
+  if(sendNotification
+    && currentHour>=allowNotificationHourStart
+    && currentHour<=allowNotificationHourEnd){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function saveNewItemsFile(){
+  fs.writeFile(newItemsfile, JSON.stringify(newItems), 'utf8', function(){});
+}
+
+function getDateTimeString(){
+  var date = new Date();
+  return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+}
+
+function writeLog(msg){
+  console.log(getDateTimeString() + ': ' + msg);
+}
 
 function checkItems(){
-  console.log('Checking...');
+  var currentHour = new Date().getHours();
+  writeLog('Checking...');
   // For debug.
   // items = JSON.parse(fs.readFileSync(itemfile, 'utf8'));
-  console.log(binPath);
-  childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-    var msg = stdout;
-    // console.log(msg);
-    var objs = JSON.parse('{'+msg.substring(0, msg.length - 2) + '}');
-    for(var obj in objs){
-    // console.log(objs[obj]);
-      if(items[obj] == undefined){
-        items[obj] = objs[obj];
-        newItems[obj] = objs[obj];
-        if(sendNotification){
-          bot.say(
-            {
-              // text: JSON.stringify(newItems[obj]),
-              text: newItems[obj].title + ' - ' + newItems[obj].price + ' - ' + newItems[obj].age + ' - '
-              + newItems[obj].mileage + ' - ' + 'http://www.ksl.com/auto/listing/'+newItems[obj].data.id,
-              channel: 'U2EDZ9C3G' //ylyan
-            }
-          );
+
+  var itemsToNotify = JSON.parse(fs.readFileSync(newItemsfile, 'utf8'));
+  if(checkToSendNotificationNow(currentHour)){
+    for (var item in itemsToNotify){
+      sendSlackNotification(itemsToNotify[item]);
+    }
+    newItems = {};
+    saveNewItemsFile();
+  }
+
+  writeLog(childArgs[1]);
+  var queries = JSON.parse(fs.readFileSync(queryFile, 'utf8'));
+
+  async.forEachOf(queries, (value, key, callback) => {
+    writeLog(key);
+    sleep.sleep(3);
+}, err => {
+    if (err) console.error(err.message);
+});
+  for (var q in queries){
+    writeLog("getting data for: " + q);
+    childArgs[1] = queries[q];
+    childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+      var msg = stdout;
+      // As of 2018-08-15, there're some TypeError at the beginning of the
+      // returned data, need to get rid of it first.
+      
+      var data = '{'+msg.substring(msg.indexOf("title")-13, msg.length - 2) + '}';
+  
+      // writeLog(data);
+      var objs = JSON.parse(data);
+      items = JSON.parse(fs.readFileSync(itemfile, 'utf8'));
+      for(var obj in objs){
+        if(items[obj] == undefined){
+          items[obj] = objs[obj];
+          if(checkToSendNotificationNow(currentHour)){
+            sendSlackNotification(items[obj]);
+          }
+          else if (sendNotification) {
+            newItems[obj] = objs[obj];
+          }
         }
       }
-    }
-
-    fs.writeFile(itemfile, JSON.stringify(items), 'utf8', function(){
-      fs.writeFile(newItemsfile, JSON.stringify(newItems), 'utf8', function(){
+  
+      fs.writeFile(itemfile, JSON.stringify(items), 'utf8', function(){
+        saveNewItemsFile();
       });
+      sendNotification = true;
+      writeLog('DONE...');
+      // bot.closeRTM();
     });
-    sendNotification = true;
-    console.log('DONE...');
-    // bot.closeRTM();
-  });
+  }
 }
 
 properties.parse ('.properties', { path: true }, function (error, obj){
@@ -80,7 +144,7 @@ properties.parse ('.properties', { path: true }, function (error, obj){
   botOptions.token = obj.token;
   bot = controller.spawn(botOptions).startRTM();
   checkItems();
-  interval = setInterval(checkItems, 600000);
+  // interval = setInterval(checkItems, 600000);
 });
 
 // clearInterval(interval);
