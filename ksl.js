2486119820576592d3dcec181da5b5594e799d0c
Yuelin37 <yuelinyan@gmail.com>
2019-07-23 22:05:32 -0600

    righ way

--- ksl.js
+++ ksl.js
@@ -1,59 +1,65 @@
-var Botkit = require('botkit');
+'use strict';
+var request = require("request");
 var path = require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs-prebuilt');
 var binPath = phantomjs.path;
 var fs = require('fs');
-var properties = require ('properties');
-var async = require('async');
-var sleep = require('sleep');
+var properties = require('properties');
+// var sleep = require('sleep');
 
 var url_query;
 var childArgs;
 var interval;
-var sendNotification = false;
-var allowNotificationHourStart = 19;
-var allowNotificationHourEnd = 20;
+var allowNotificationHourStart = 17;
+var allowNotificationHourEnd = 21;
 
-var controller = Botkit.slackbot({
-  debug: false,
-});
-var botOptions = {};
-var bot;
 
-var itemfile = __dirname + '/items.json';
-var items = {};
-if (!fs.existsSync(itemfile)) {
-  fs.writeFileSync(itemfile, '{}');
-}else {
-  items = JSON.parse(fs.readFileSync(itemfile, 'utf8'));
-}
-
-var newItemsfile = __dirname + '/newItems.json';
 var queryFile = __dirname + '/query.json';
-var newItems = {};
-if (!fs.existsSync(newItemsfile)) {
-  fs.writeFileSync(newItemsfile, '{}');
-}
-else {
-  newItems = JSON.parse(fs.readFileSync(newItemsfile, 'utf8'));
-}
 
-function sendSlackNotification(item){
-  bot.say(
+
+function sendMessage(msg) {
+  let options = {
+    method: 'POST',
+    url: 'https://hooks.slack.com/services/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',
+    headers:
     {
-      // text: JSON.stringify(newItems[obj]),
-      text: item.title + ' - ' + item.price + ' - ' + item.age + ' - '
-      + item.mileage + ' - ' + 'http://www.ksl.com/auto/listing/'+item.data.id,
-      channel: 'U2EDZ9C3G' //ylyan
+      'cache-control': 'no-cache',
+      Connection: 'keep-alive',
+      'accept-encoding': 'gzip, deflate',
+      Accept: '*/*',
+      'Content-type': 'application/json'
     }
-  );
+  };
+  // writeLog(options.body);
+  let body = '{"text":"' + msg + '"}';
+  // let body = '{' + msg + '}';
+  options.body = body;
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log("Message Sent --- " + msg);
+  });
 }
 
-function checkToSendNotificationNow(currentHour){
-  if(sendNotification
-    && currentHour>=allowNotificationHourStart
-    && currentHour<=allowNotificationHourEnd){
+function sendSlackNotification(newItemsfile) {
+  fs.readFile(newItemsfile, 'utf8', (err, newItemsFileData) => {
+    if (err) {
+      throw err;
+    }
+    let newItems = JSON.parse(newItemsFileData);
+    for (let item in newItems) {
+      sendMessage(newItems[item].title + ' - ' + newItems[item].price + ' - ' + newItems[item].age + ' --- http://www.ksl.com/auto/listing/' + item);
+    }
+
+    // Clear newItemsfile
+    fs.writeFileSync(newItemsfile, '{}');
+  });
+}
+
+function checkToSendNotificationNow() {
+  var currentHour = new Date().getHours();
+  if (currentHour >= allowNotificationHourStart
+    && currentHour <= allowNotificationHourEnd) {
     return true;
   }
   else {
@@ -61,90 +67,100 @@ function checkToSendNotificationNow(currentHour){
   }
 }
 
-function saveNewItemsFile(){
-  fs.writeFile(newItemsfile, JSON.stringify(newItems), 'utf8', function(){});
+function saveNewItemsFile(newItemsfile, newItems) {
+
+  writeLog("writing: " + newItemsfile);
+  writeLog("writing: " + JSON.stringify(newItems));
+  fs.writeFile(newItemsfile, JSON.stringify(newItems), 'utf8', () => {
+    // writeLog("New Items Saved: " + newItemsfile);
+    if (checkToSendNotificationNow()){
+      sendSlackNotification(newItemsfile);
+    }
+  });
 }
 
-function getDateTimeString(){
+function getDateTimeString() {
   var date = new Date();
   return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
 }
 
-function writeLog(msg){
+function writeLog(msg) {
   console.log(getDateTimeString() + ': ' + msg);
 }
 
-function checkItems(){
-  var currentHour = new Date().getHours();
-  writeLog('Checking...');
+function checkItems() {
+  writeLog('===============================================');
+
   // For debug.
   // items = JSON.parse(fs.readFileSync(itemfile, 'utf8'));
 
-  var itemsToNotify = JSON.parse(fs.readFileSync(newItemsfile, 'utf8'));
-  if(checkToSendNotificationNow(currentHour)){
-    for (var item in itemsToNotify){
-      sendSlackNotification(itemsToNotify[item]);
-    }
-    newItems = {};
-    saveNewItemsFile();
-  }
-
-  writeLog(childArgs[1]);
   var queries = JSON.parse(fs.readFileSync(queryFile, 'utf8'));
 
-  async.forEachOf(queries, (value, key, callback) => {
-    writeLog(key);
-    sleep.sleep(3);
-}, err => {
-    if (err) console.error(err.message);
-});
-  for (var q in queries){
-    writeLog("getting data for: " + q);
+  for (let q in queries) {
+    writeLog("Start --- getting data for: " + q);
     childArgs[1] = queries[q];
-    childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-      var msg = stdout;
+    childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
+      let msg = stdout;
       // As of 2018-08-15, there're some TypeError at the beginning of the
       // returned data, need to get rid of it first.
-      
-      var data = '{'+msg.substring(msg.indexOf("title")-13, msg.length - 2) + '}';
-  
-      // writeLog(data);
-      var objs = JSON.parse(data);
-      items = JSON.parse(fs.readFileSync(itemfile, 'utf8'));
-      for(var obj in objs){
-        if(items[obj] == undefined){
-          items[obj] = objs[obj];
-          if(checkToSendNotificationNow(currentHour)){
-            sendSlackNotification(items[obj]);
-          }
-          else if (sendNotification) {
-            newItems[obj] = objs[obj];
-          }
-        }
+      let data = '{' + msg.substring(msg.indexOf("title") - 13, msg.length - 2) + '}';
+
+      let objs = JSON.parse(data);
+
+      let itemfile = __dirname + '/items-' + q + '.json';
+      let newItemsfile = __dirname + '/newItems-' + q + '.json';
+      let foundNewItem = false;
+
+      if (!fs.existsSync(itemfile)) {
+        fs.writeFileSync(itemfile, '{}');
       }
-  
-      fs.writeFile(itemfile, JSON.stringify(items), 'utf8', function(){
-        saveNewItemsFile();
+      if (!fs.existsSync(newItemsfile)) {
+        fs.writeFileSync(newItemsfile, '{}');
+      }
+
+      fs.readFile(newItemsfile, 'utf8', (err, newItemsFileData) => {
+        if (err) {
+          throw err;
+        }
+        // writeLog(newItemsfile + " loaded");
+        fs.readFile(itemfile, 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          // writeLog(itemfile + " loaded");
+          // writeLog(data);
+          let items = JSON.parse(data);
+          let newItems = JSON.parse(newItemsFileData);
+          for (let obj in objs) {
+            if (items[obj] == undefined) {
+              items[obj] = objs[obj];
+              newItems[obj] = objs[obj];
+              foundNewItem = true;
+            }
+          }
+
+          fs.writeFile(itemfile, JSON.stringify(items), 'utf8', function () {
+            writeLog('Done --- getting data for: ' + q);
+          });
+          if(foundNewItem){
+            saveNewItemsFile(newItemsfile, newItems);
+          }
+        });
       });
-      sendNotification = true;
-      writeLog('DONE...');
-      // bot.closeRTM();
     });
   }
 }
 
-properties.parse ('.properties', { path: true }, function (error, obj){
-  if (error) return console.error (error);
+properties.parse('.properties', { path: true }, function (error, obj) {
+  if (error) return console.error(error);
 
   url_query = obj.url;
   childArgs = [
     path.join(__dirname, 'getlist.js'),
     url_query
   ];
-  botOptions.token = obj.token;
-  bot = controller.spawn(botOptions).startRTM();
   checkItems();
-  // interval = setInterval(checkItems, 600000);
+  interval = setInterval(checkItems, 600000);
 });
 
 // clearInterval(interval);
